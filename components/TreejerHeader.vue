fae7c50f2cb82bd67fcf952839168e892dfecb87
Iraj Habibzadeh <iraj.habibzadeh70@gmail.com>
2020-04-10 19:12:05 +0300

    fix store user login

--- components/TreejerHeader.vue
+++ components/TreejerHeader.vue
@@ -2,42 +2,71 @@
   <div class="headers container">
     <client-only>
       <b-navbar toggleable="md">
-        <b-navbar-brand class="pointer-event position-relative" :to="localePath('home')">
-          <img class="img-fluid pointer-event" alt="logo" name="treejer" src="/logo/TreejerLogo@3x.png">
+        <b-navbar-brand
+          class="pointer-event position-relative"
+          :to="localePath('home')"
+        >
+          <img
+            class="img-fluid pointer-event"
+            alt="logo"
+            name="treejer"
+            src="/logo/TreejerLogo@3x.png"
+          />
         </b-navbar-brand>
 
-        <b-navbar-toggle target="nav-collapse">
+        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
-        </b-navbar-toggle>
-
-        <b-collapse class="text-right justify-content-between" id="nav-collapse" is-nav>
-
+        <b-collapse
+          class="text-right justify-content-between"
+          id="nav-collapse"
+          is-nav
+        >
           <!-- Right aligned nav items -->
 
           <b-navbar-nav class="header-menu">
-            <b-nav-item href="#">About</b-nav-item>
-            <b-nav-item href="#">Blog</b-nav-item>
-            <b-nav-item href="#">For Business</b-nav-item>
-            <b-nav-item href="#">Find My Tree</b-nav-item>
+            <b-nav-item v-if="!$store.state.user" :class="{ 'active': activeIndex === index }"
+                        v-for="(item,index) in items" :name="item.name" :key="index" :to="item.href"
+                        @click="activeMenu(item,index)">
+              {{item.name}}
+            </b-nav-item>
           </b-navbar-nav>
           <b-navbar-nav>
-            <b-nav-form v-if="!user">
-              <b-button @click.prevent="login" class="connect-button m-auto" type="submit">Connect Wallet</b-button>
-              <img alt="tree" name="tree" src="/tree.svg" class="img-fluid tree pointer-event">
+            <b-nav-form v-if="isLoggedIn !== 'test'">
+              <b-button
+                @click.prevent="login"
+                class="connect-button m-auto"
+                type="submit"
+              >Connect Wallet
+              </b-button
+              >
+              <img
+                alt="tree"
+                name="tree"
+                src="/tree.svg"
+                class="img-fluid tree pointer-event"
+              />
             </b-nav-form>
-            <b-nav-form v-if="user">
-              <b-button @click.prevent="logout" class="connect-button m-auto" type="submit">{{token}}</b-button>
-              <img alt="tree" name="tree" src="/tree.svg" class="img-fluid tree pointer-event">
+            <b-nav-form v-if="isLoggedIn === 'test'">
+              <b-button
+                @click.prevent="logout"
+                class="connect-button m-auto"
+                type="submit"
+              >{{ token }}
+              </b-button
+              >
+              <img
+                alt="tree"
+                name="tree"
+                src="/tree.svg"
+                class="img-fluid tree pointer-event"
+              />
             </b-nav-form>
-
 
             <!-- Using 'button-content' slot -->
           </b-navbar-nav>
         </b-collapse>
       </b-navbar>
     </client-only>
-
-
   </div>
 </template>
 
@@ -47,9 +76,21 @@
     data() {
       return {
         formError: null,
-        userName: 'test',
+        userName: "test",
         user: false,
-        token: '',
+        token: "",
+        activeIndex: 0,
+        items: [
+          {name: "About", step: 1, href: 'about'},
+          {name: "Blog", step: 2, href: 'blog'},
+          {name: "For Business", step: 3, href: 'forBusiness'},
+          {name: "Find My Tree", step: 4, href: 'findMyTree'},
+        ]
+      };
+    },
+    computed: {
+      isLoggedIn() {
+        return this.$store.state.user;
       }
     },
     methods: {
@@ -60,74 +101,119 @@
           href: href
         });
       },
-
-      login() {
-        // this.$store.state.user = true
-        this.user = true
-        this.token = 'test'
-        this.loginToast('success', 'login', 'test')
-
-
+      login: function () {
+        let userName = this.userName;
+        this.$store
+          .dispatch("login", {userName})
+          .then(() => {
+            this.$cookies.set('token', true)
+            this.loginToast('success',)
+            this.token = 'test'
+            this.$router.push("myForest")
+          })
+          .catch(err => console.log(err));
+      },
+      logout() {
+        this.$store.dispatch("logout").then(() => {
+          this.$router.push("/");
+        });
       },
 
-      async logout() {
-        // this.$store.state.user = false
-        this.user = false
-        this.token = ''
-        this.loginToast('danger', 'logout')
-      }
+      activeMenu(item, index) {
+
+        this.activeIndex = index;
+      },
 
     }
-  }
+  };
 </script>
 
-<style  lang="scss">
-    .headers {
-      .header-menu {
+<style lang="scss">
+  .headers {
+    .navbar-nav.header-menu {
+      margin-left: 58px;
+    }
 
-        font-size: 14px;
-        font-weight: bolder;
-        color: #616161;
-        padding-top: 5px;
+    .header-menu {
+      font-size: 14px;
+      font-weight: bolder;
+      color: #616161;
+      padding-top: 5px;
 
-        .nav-item {
-          padding: 15px 35px 25px 0;
+      .nav-item {
+        padding: 15px 35px 25px 0;
+
+        a {
+          font-family: Montserrat;
+          font-size: 14px;
+          font-weight: 500;
+          position: relative;
         }
 
+        a:after {
+          position: absolute;
+          content: '';
+          border-right: solid 1px gray;
+          width: 3px;
+          height: 50%;
+          right: -23%;
+          align-items: center;
+          vertical-align: middle;
+
+        }
+
+        li:last-child > a {
+          display: none;
+
+        }
       }
 
-      height: 103px;
-      background: #faf8f1;
+      .nav-item:last-child > a:after {
+        display: none;
 
-      .connect-button {
-        width: 128px;
-        height: 32px;
-        border-radius: 20px;
-        background-color: #424242;
-        font-family: Montserrat;
-        font-size: 12px;
-        font-weight: 500;
-        font-stretch: normal;
-        font-style: normal;
-        line-height: 1.25;
-        letter-spacing: normal;
-        color: #f5f5f5;
       }
 
+      .nav-item.active {
+        a {
+          color: #5a9d79;
 
-      img {
-        width: 71px;
-        height: 71px;
-        object-fit: contain;
-      }
+        }
 
-      img.tree {
-        width: 27px;
-        height: 36px;
-        object-fit: contain;
-        margin: 0 0px 0px 20px;
+        a:after {
+          border-right: solid 1px #5a9d79;
+        }
       }
     }
 
+    height: 103px;
+    background: #faf8f1;
 
+    .connect-button {
+      width: 128px;
+      height: 32px;
+      border-radius: 20px;
+      background-color: #424242;
+      font-family: Montserrat;
+      font-size: 12px;
+      font-weight: 500;
+      font-stretch: normal;
+      font-style: normal;
+      line-height: 1.25;
+      letter-spacing: normal;
+      color: #f5f5f5;
+    }
+
+    img {
+      width: 71px;
+      height: 71px;
+      object-fit: contain;
+    }
+
+    img.tree {
+      width: 27px;
+      height: 36px;
+      object-fit: contain;
+      margin: 0 0px 0px 20px;
+    }
+  }
 </style>
