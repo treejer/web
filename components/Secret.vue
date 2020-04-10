a90d768f5cbbd87eaa060d0af82592e9aebabcae
Iraj Habibzadeh <iraj.habibzadeh70@gmail.com>
2020-04-10 19:10:38 +0300

    fixe route name home to /

---
+++ components/Secret.vue
@@ -0,0 +1,13 @@
+<template>
+    $END$
+</template>
+
+<script>
+    export default {
+        name: "Secret"
+    }
+</script>
+
+<style scoped>
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,27 +1,98 @@
 <template>
-
-
-  <div class="loading">  <b-spinner variant="success" type="grow" label="Spinning"></b-spinner>
-  </div>
+    <section :page="$route.name" class="position-relative pt-5 col-md-12 home">
+      <div class="container-fluid">
+        <div class="row article">
+          <div class="col-md-8 col-12">
+            <div class="row">
+              <div class="col-12 title">
+                <h1>
+                  A more <span class="tr-green">affordable</span>Climate Action
+                  through financial inclusion of the rural communities
+                </h1>
+              </div>
+              <div class="col-12 form-group mt-5">
+                <button
+                  class="btn-lg btn-green pointer-event"
+                  @click="$router.push('/myForest')"
+                >
+                  Plant One Tree
+                </button>
+                <button class="btn-lg btn-outline-green pointer-event">
+                  Explore Forests
+                </button>
+              </div>
+              <div class="col-12 d-flex tr-status mt-3 ">
+                <div class="planet-tree pt-2  ">
+                  <p class="param-sm tr-black font-weight-bold">Planted Trees</p>
+                  <p class="title-md tr-green font-weight-bolder">159</p>
+                </div>
+                <div class="rural-communities pt-2">
+                  <p class="param-sm tr-black font-weight-bold">
+                    Rural Communities
+                  </p>
+                  <p class="title-md tr-green font-weight-bolder">2</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
 </template>
-
 <script>
-    export default {
-      name: "index.vue",
-      created() {
-          this.$router.push('/myForest')
-      }
-    }
+  export default {
+    name: "index",
+    props: {},
+    created() {}
+  };
 </script>
 
-<style scoped>
-.loading{
-  width: 100vw;
-  height: 100vh;
-  position: fixed;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  align-self: center;
-}
+<style lang="scss">
+  .home {
+    .loading{
+      width: 100vw;
+      height: 100vh;
+      position: fixed;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      align-self: center;
+    }
+    background-image: url(../assets/images/home/treejerStory.png);
+    background-size: cover;
+    .planet-tree {
+      border-right: solid 1px #67b68c;
+      padding: 0 15px;
+    }
+    .rural-communities {
+      padding: 0 15px;
+    }
+
+    .banner {
+      z-index: +9;
+    }
+
+    .title {
+      h1 {
+        font-size: 28px;
+        font-weight: bold;
+        line-height: 48px;
+        color: #616161;
+
+        span {
+          font-size: 28px;
+          font-weight: bold;
+          color: #5a9d79;
+          padding: 0 5px;
+        }
+      }
+    }
+  }
+  @media (max-width:768px) {
+    .home {
+      .banner {
+        display: none;
+      }
+    }
+  }
 </style>
