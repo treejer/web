061fd84663e9a1c1c70ad9d94821cef3c66a9879
Iraj Habibzadeh <iraj.habibzadeh70@gmail.com>
2020-06-14 20:01:00 +0430

    resolve backlog

--- pages/findMyTree/stepOne.vue
+++ pages/findMyTree/stepOne.vue
@@ -11,7 +11,7 @@
               class="param-md font-weight-bold"
             >
               <p>{{ item.name }}</p>
-              <p :class="{ active: activeIndex === index }">{{ item.step }}</p>
+              <p :class="{ active: activeIndex === index }">{{ item.step  }}</p>
             </li>
           </ul>
         </div>
@@ -216,147 +216,6 @@
             </div>
           </div>
         </div>
-        <div class="col-12 step-two" v-if="activeIndex === 1">
-          <div class="row">
-            <div class="col-12 mt-5 justify-content-center text-center">
-              <h1 class="title-sm font-weight-bolder text-center ">
-                Select Wallet
-              </h1>
-            </div>
-            <div
-              class="col-12 text-center col-lg-4 d-none  d-md-block align-items-center align-self-center  h-100"
-            >
-              <p class="param-md  tr-gray-three font-weight-bold">Need help?</p>
-              <p
-                class="pointer-event tr-green"
-                @click="localePath('contactUs')"
-              >
-                support@treejer.com
-              </p>
-            </div>
-            <div
-              class="col-12 col-lg-4 mt-3 justify-content-center text-center "
-            >
-              <ul class="wallets tr-gray-three list-style-none">
-                <li v-for="(item, index) in wallets" class="pointer-event">
-                  <p
-                    @click="activeWallets(item, index)"
-                    :class="{ active: activeWallet === index }"
-                    :key="index"
-                    :name="item.name"
-                  >
-                    {{ item.name }}
-                  </p>
-                </li>
-              </ul>
-              <p class="param  mt-3 tr-gray-five">New to Ethereum?</p>
-              <p class="param   tr-green">Learn more about wallets</p>
-              <button
-                class="btn-green param-md btn-lg mt-5 font-weight-bolder text-white "
-                @click="activeIndex = 2"
-              >
-                CONNECT
-              </button>
-            </div>
-            <div
-              class="col-12 col-lg-4 d-none  d-lg-block flex-row align-items-center align-self-center  h-100"
-            >
-              <hr />
-              <p class="param-md font-weight-bold  tr-gray-three text-center">Total</p>
-              <p class="title-md text-center tr-gray-three font-weight-bolder">
-                $173.00
-              </p>
-              <hr />
-            </div>
-          </div>
-        </div>
-        <div class="col-12 step-three" v-if="activeIndex === 2">
-          <div class="row">
-            <div class="col-12 mt-5 justify-content-center text-center">
-              <h1 class="title-sm font-weight-bolder text-center ">
-                Confirm Payment Information
-              </h1>
-              <p class="tr-gray-three mt-5">
-                You're adding <span class="tr-green">{{ "1 tree" }}</span> to
-                your forest!
-              </p>
-            </div>
-
-            <div
-              class="col-12 col-lg-4 d-none  d-md-flex flex-row align-items-center align-self-center  h-100"
-            >
-              <p class="param-md  tr-gray-three font-weight-bold">Need help?</p>
-              <p
-                class="pointer-event tr-green"
-                @click="localePath('contactUs')"
-              >
-                support@treejer.com
-              </p>
-            </div>
-            <div
-              class="col-12 col-lg-4 mt-3 justify-content-center text-center "
-            >
-              <div class="tr-table tr-card">
-                <div class="row">
-                  <div class="col-lg-5">
-                    <p class="param tr-gray-four">Total</p>
-                    <p class="param tr-gray-four">Payment Fee</p>
-                  </div>
-                  <div class="col-lg-7">
-                    <p
-                      class="param tr-gray-five font-weight-light position-absolute"
-                    >
-                      <span>ETH</span><span>USD</span>
-                    </p>
-
-                    <p class="param tr-gray-four">
-                      <span id="eth">1.5639</span
-                      ><span class="usd">$173.00</span>
-                    </p>
-                    <p class="param tr-gray-four">
-                      <span id="eths">0.0024</span
-                      ><span class="usds">0.0024</span>
-                    </p>
-                  </div>
-                </div>
-              </div>
-              <button
-                @click="activeIndex = 3"
-                class="btn-green param-md btn-lg mt-5 font-weight-bolder text-white"
-              >
-                CONFIRM
-              </button>
-            </div>
-            <div
-              class="col-12 col-lg-4 d-none d-md-block d-flex flex-row align-items-center align-self-center  h-100"
-            >
-              <hr />
-              <p class="title-sm  tr-gray-three text-center">Total</p>
-              <p class="title-md text-center tr-gray-three font-weight-bolder">
-                $173.00
-              </p>
-              <hr />
-            </div>
-          </div>
-        </div>
-        <div class="col-12 final-step justify-content-center text-center"
-             v-if="activeIndex === 3"
-        >
-          <img
-            src="~/assets/images/myforest/final-step.png"
-            class="img-fluid mt-5"
-            alt="final-step"
-          />
-          <h1 class="title-sm font-weight-bold mt-5 tr-gray-two">
-            Congratulations, your forest just got bigger!!
-          </h1>
-          <p class="mt-3 tr-gray-three">
-            Rural communities are notified to plant your trees.
-          </p>
-          <nuxt-link class="tr-green param pt-5" :to="localePath('myForest')"
-          >Go back to My Forest</nuxt-link
-          >
-        </div>
       </div>
     </div>
   </section>
@@ -366,8 +225,10 @@
 <script>
     export default {
         name: "stepOne",
+
       data(){
           return{
+            activeIndex:0,
             steps: [
               { name: "Processing", step: 1 },
               { name: "Pending Plantation", step: 2 },
