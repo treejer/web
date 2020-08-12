d7ee3b695226bdf2883e89b6e867de46c76a6a51
Iraj Habibzadeh <iraj.habibzadeh70@gmail.com>
2020-08-12 18:06:16 +0430

    fix dashboard meta mask network name error

--- pages/help.vue
+++ pages/help.vue
@@ -82,7 +82,7 @@ export default {
               x.parentNode.insertBefore(e, x);
               t.Tribe('boot', {portal: i});
             }
-          })(window, document, 'https://treejer.com/')
+          })(window, document, 'https://community.tribe.so')
         }
       ]
     }
