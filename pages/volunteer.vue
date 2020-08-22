400501a7fb119db6144112e2273711828db37182
Iraj Habibzadeh <iraj.habibzadeh70@gmail.com>
2020-08-22 11:34:08 +0430

    fix bugs

--- pages/volunteer.vue
+++ pages/volunteer.vue
@@ -1,22 +1,9 @@
 <template>
   <section :page="$route.name" class="position-relative pt-5 col-12  volunteer" style="min-height: 88vh">
     <client-only>
-      <BSpinner class="mr-2" type="grow" size="lg" v-if="loading">
-      </BSpinner>
+  
+      <iframe @ended="loading =false" src="https://form.typeform.com/to/INs6wu" class="border-0" height="100%" width="100%" title="Iframe typeform"></iframe>
 
-      <div class="typeform-widget" data-url="https://form.typeform.com/to/INs6wu"
-           style="width: 100%; height: 100%;"></div>
-      <script> (function () {
-        var qs, js, q, s, d = document, gi = d.getElementById, ce = d.createElement, gt = d.getElementsByTagName,
-          id = "typef_orm", b = "https://embed.typeform.com/";
-        if (!gi.call(d, id)) {
-          js = ce.call(d, "script");
-          js.id = id;
-          js.src = b + "embed.js";
-          q = gt.call(d, "script")[0];
-          q.parentNode.insertBefore(js, q)
-        }
-      })() </script>
 
     </client-only>
   </section>
@@ -27,8 +14,12 @@ export default {
   name: "volunteer",
   data() {
     return {
-      loading: false
+      loading: true
     }
+  },
+  mounted() {
+
+
   }
 }
 </script>
