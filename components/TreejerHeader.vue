3069397b7b04161cd732a1bda8c4c163c21abf19
Iraj Habibzadeh <iraj.habibzadeh70@gmail.com>
2020-09-02 22:46:55 +0430

    fix bugs

--- components/TreejerHeader.vue
+++ components/TreejerHeader.vue
@@ -115,23 +115,20 @@ export default {
         })
       },
       async accountChange() {
-        if(this.$cookies.get('metamsak')){
-          if (process.client) {
-            let self = this
-            if (typeof window.ethereum !== 'undefined') {
-              await window.ethereum.on('accountsChanged', function (accounts) {
-                // Time to reload your interface with accounts[0]!
-                self.$store.commit('SET_USER', accounts[0])
-                self.$cookies.set('account', accounts[0])
+        if(this.$cookies.get('walletName') === 'metamask'){
+          if(process.client){
+            let self =this
+            await window.ethereum.on('accountsChanged', function (accounts) {
+              if(self.$cookies.get(accounts) !== accounts[0]){
+                self.$store.commit('SET_USER',accounts[0])
+                self.$cookies.set('account',accounts[0])
                 const history = self.$router.history.current.fullPath
-                self.$router.go(history)
+                self.$router.push(history)
+                self.$forceUpdate()
+              }
 
-              });
-            }
-        }
-
-
-        }
+            });
+          }}
       },
       activeMenu(item, index) {
         if (item.name === 'Blog') {
