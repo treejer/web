f75020deac734cc4b14d79708080a860fc6539db
iraj habibzadeh <iraj.habibzadeh70@gmail.com>
2022-02-16 08:59:11 +0330

    fix layouts page name

--- layouts/checkout.vue
+++ layouts/checkout.vue
@@ -1,5 +1,5 @@
 <template>
-  <div :class="currentRouteName"  style="min-height: 100vh">
+  <div :class="$route.name"  style="min-height: 100vh">
       <DashboardHeader/>
       <div class="container"  style="min-height: 85vh">
         <div class="row">
@@ -23,11 +23,7 @@
       return {
       };
     },
-    computed: {
-      currentRouteName() {
-        return this.$route.path;
-      }
-    },
+
     methods: {
       onComplete(data) {
         this.account = data;
