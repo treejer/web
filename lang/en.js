209638bcc11980113cc21bbc3f2e527426c4fb0a
Amir Habibzadeh <habibzade.amir@gmail.com>
2022-04-16 21:00:29 +0300

    crowdin branch initialized

---
+++ lang/en.js
@@ -0,0 +1,22 @@
+export default {
+    title: 'Treejer',
+    homepage: {
+        title: 'Planters Without Borders',
+        maintextprefix: 'A more',
+        maintexts: {
+            affordable: ' affordable',
+            sustainable: ' sustainable',
+            transparent: ' transparent',
+            impactful: ' impactful',
+            inclusive: ' inclusive',
+            rewarding: ' rewarding',
+            gamified: ' gamified',
+            measurable: ' measurable',
+        },
+        maintextmid: 'Climate Action through',
+        maintextsuffix: 'financial inclusion of rural communities.'
+    },
+    aboutus: {
+        title: 'About'
+    }
+}
