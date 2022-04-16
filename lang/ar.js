7faa87cd694320b34099f79e3b5f912f0dd3fc9a
Amir Habibzadeh <habibzade.amir@gmail.com>
2022-04-16 21:02:33 +0300

    New translations en.js (Arabic)

---
+++ lang/ar.js
@@ -0,0 +1,20 @@
+export default {
+    title: 'Treejer',
+    homepage: {
+        title: 'Planters Without Borders',
+        maintextprefix: 'A more',
+        maintexts: {
+            affordable: ' affordable',
+            sustainable: ' sustainable',
+            transparent: ' transparent',
+            impactful: ' impactful',
+            inclusive: ' inclusive',
+            rewarding: ' rewarding',
+            gamified: ' gamified',
+            measurable: ' measurable'
+        },
+        maintextmid: 'Climate Action through',
+        maintextsuffix: 'financial inclusion of rural communities.'
+    },
+    aboutus: { title: 'About' }
+};
